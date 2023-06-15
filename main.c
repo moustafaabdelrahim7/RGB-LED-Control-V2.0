1295b4eba331fa2acee7bb4c810bfc838a2b415e
moustafaabdelrahim7 <128411699+moustafaabdelrahim7@users.noreply.github.com>
2023-06-15 10:58:27 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "sys_tick.h"
 
 extern const str_port_config_t	str_port_config_arr[4];
-uint8_t u8_lv_Counter=1;
+uint8_t u8_gv_Counter=1;
 int main(void)
 {	
 	LED_INIT(str_port_config_arr);
@@ -16,21 +16,21 @@ int main(void)
 			{
 				SYS_TICK_ENABLE(SYSTEM_CLK,SYS_TICK_WITH_INT,1000);
 					
-					if(u8_lv_Counter==1)
+					if(u8_gv_Counter==1)
 					{
 							LED_ON(PORT_F,PORT_PIN_1);
 					}
-					else if(u8_lv_Counter==2)
+					else if(u8_gv_Counter==2)
 					{
 						LED_ON(PORT_F,PORT_PIN_3);
 					
 					}
-					else if(u8_lv_Counter==3)
+					else if(u8_gv_Counter==3)
 					{
 						LED_ON(PORT_F,PORT_PIN_2);
 					
 					}
-					else if(u8_lv_Counter==4)
+					else if(u8_gv_Counter==4)
 					{
 						LED_ON(PORT_F,PORT_PIN_1);
 						LED_ON(PORT_F,PORT_PIN_3);
@@ -49,10 +49,10 @@ void SysTick_Handler(void)
 	LED_OFF(PORT_F,PORT_PIN_1);
 	LED_OFF(PORT_F,PORT_PIN_3);
 	LED_OFF(PORT_F,PORT_PIN_2);
-	u8_lv_Counter++;
+	u8_gv_Counter++;
 	SYS_TICK_Disable();
-	if(u8_lv_Counter==5)
+	if(u8_gv_Counter==5)
 	{
-		u8_lv_Counter=1;
+		u8_gv_Counter=1;
 	}
 }
